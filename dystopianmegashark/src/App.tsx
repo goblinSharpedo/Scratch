0062160b3d7174d308f3e8ccdff5182df9cec395
CarlborHal <grwlp5k@gmail.com>
2025-04-26 14:54:57 -0400

    Updated form to comply with updated schema

--- dystopianmegashark/src/App.tsx
+++ dystopianmegashark/src/App.tsx
@@ -23,8 +23,10 @@ function App() {
     <h1>Not-a-Dystopian-Society-Social-Credit-Score-App</h1>
     <h2>(because if Amazon can do it, so can we)</h2>
     <form onSubmit={handleSubmit} className='createForm'>
-      <input type='text' placeholder='Name' name = 'name'></input>
-      <input type='text' placeholder='role' name = 'role'></input>
+      <input type='text' placeholder='First Name' name = 'firstName'></input>
+      <input type='text' placeholder='Last Name' name = 'lastName'></input>
+      <input type='text' placeholder='Age' name = 'age'></input>
+      <input type='text' placeholder='Role' name = 'role'></input>
       <button type='submit'>Submit Citizen for Processing</button>
     </form>
     </>
