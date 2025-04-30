ad9593b21bed67a3cfce153bc18aee28f86e246d
Jamie Tait <taitjamie@outlook.com>
2025-04-30 11:13:02 -0700

    report posting a console.log but error in fetch to save to DB

--- dmsfront/src/components/home/home.tsx
+++ dmsfront/src/components/home/home.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import styles from './home.module.css';
 
+
 function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.mainContent}>
-        <h1 className={styles.title}>INSERT NEW APP NAME HERE</h1>
-        <img className={styles.logo} src="" alt="Cat Snake Logo" />
+        <h1 className={styles.title}>Not-a-Dystopian-Society-Social-Credit-Score-App</h1>
+        <a href="/report">
+        <img className={styles.logo} src="src/assets/catsnake_kind.png" alt="cat snake logo"/>
+        </a>
         <p className={styles.tagLine}>We're pretty sure this a flawless idea with no consequences</p>
       </main>
     </div>
--- dmsfront/src/components/report/report.tsx
+++ dmsfront/src/components/report/report.tsx
@@ -1,31 +1,46 @@
-import  {FormEvent} from 'react'
+import {FormEvent} from 'react'
 
 import styles from './report.module.css'
 
 function Report() {
 //make post request
-  const handleSubmit = async (event: FormEvent<HTMLFormElement>) =>{
-    event.preventDefault()
+
+ async function handleSubmit(event: FormEvent<HTMLFormElement>){
+    event.preventDefault();
+    const url = 'http://localhost:3000/employee';
     const formData = new FormData(event.currentTarget);
     const formObject = Object.fromEntries(formData.entries());
-    console.log(formObject);
-    await fetch(import.meta.env.VITE_SERVER_EMPLOYEE_CREATOR, { //TODO: where should this fetch point to now?
+
+    console.log('formObject:', formObject);
+
+    try{
+      const res = await fetch(url, {
       method:'POST',
-      headers:{"Content-Type": "application/json",
+      headers:{"Content-Type": "application/json"},
       body: JSON.stringify(formObject)
+      })
+      if(!res.ok){
+        throw new Error('Report submission failed!')
       }
-    })
+    }
+    catch(err){
+      console.error('Error in Report.handleSubmit', err)
+    }
   }
+  
 
   return (
     <>
     <div className={styles.createForm}>
-    <form onSubmit={handleSubmit} className='createForm'>
-      <input type='text' placeholder='First Name' name = 'firstName'/>
-      <input type='text' placeholder='Last Name' name = 'lastName'/>
-      <input type='text' placeholder='Role' name = 'role'/>
-      <button type='submit'>Submit Colleague for Processing</button>
-    </form>
+      <img className={styles.logo} src="src/assets/catsnake.png" alt="dark cat snake logo"/>
+      <h2>Great Compliance, Valued Employee!</h2>
+      <form onSubmit={handleSubmit} className='createForm'>
+        <input type='text' placeholder='First Name' name = 'firstName'/>
+        <input type='text' placeholder='Last Name' name = 'lastName'/>
+        <input type='text' placeholder='Role' name = 'role'/>
+        <input type='text' placeholder='Your Name' name = 'age'/>
+        <button type='submit'>Submit Colleague for Processing</button>
+      </form>
     </div>
     </>
   )
